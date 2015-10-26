f3a2654c0e20aee4f0273d477c660640ae4e7564
BrunaNett <beedeveloping@gmail.com>
2015-10-26 15:56:12 -0400

    courses button working from instructors page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,3 +1,38 @@
 class CoursesController < ApplicationController
+
+	def index
+    @courses = Course.all 
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private 
+
+    def set_course 
+      @course = Course.find(params[:id])
+    end
+
+    def course_params
+      params.require(:course).permit(:name)
+    end
   
 end
--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,49 +1,64 @@
 class InstructorsController < ApplicationController
 
-	def index
-		@instructor = Instructor.all
-	end
- 
-  	def show
-    	@instructor = Instructor.find(params[:id])
+  def index
+  	@instructors = Instructor.all
+  end
+   
+  def show
+     @instructor = Instructor.find(params[:id])
+   end
+
+  def new
+    @instructor = Instructor.new
+  end
+
+  def edit 
+  end
+
+  def create
+    @instructor = Instructor.new(instructor_params)
+
+     respond_to do |format|
+  	 	if @instructor.save
+  	  	  format.html { redirect_to @instructor, notice: 'New instructor has been successfully created.' }
+  			  format json { render :show, status: :created, location: @instructor }
+  		else
+  			  format.html { render :new } 
+  			  format.json { render json: @instructor.errors, status: :unprocessable_entity }
+  		end
   	end
+  end
+  	
 
-	def new
-	end
+  def update
+    @instructor = Instructor.find(parmas[:id])
+  
+    respond_to do |format|
+      if @instructor.update(instructor_params)
+          format.html { redirect_to @instructor, notice: 'Instructor was successfully updated.' }
+          format.json { render :index, status: :updated, location: @instructor}
+      else
+          format.html { render :edit}
+          format.json { render json: @instructor.erros, status: :unprocessable_entity}
+      end
+    end
+  end
 
-	def edit 
-	end
-
-	def create
-		@instructor = Instructor.new(instructor_params)
-
-		respond_to do |format|
-			if @instructor.save
-				format.html { redirect_to @instructor, notice: 'New instructor has been successfully created.' }
-				format json { render :show, status: :created, location: @instructor }
-			else
-				format.html { render :new } 
-				format.json { render json: @instructor.errors, status: :unprocessable_entity }
-			end
-		end
-	end
-	
-
-	def update
-	end
-
-	def destroy
-	end
+  def destroy
+    @instructor.destroy
+    respond_to do |format|
+      format.html { redirect_to instructor_url, notice: 'Instructor was successfully removed.'}
+      format.json { head :no_content }
+    end
+  end
 
 	private 
 
-	def set_instructor 
-		@instructor = Instructor.find(params[:id])
-	end
+		def set_instructor 
+			@instructor = Instructor.find(params[:id])
+		end
 
-	def instructor_params
-		params.require(:instructor).permit(:name)
-	end
-
-
+		def instructor_params
+			params.require(:instructor).permit(:first_name, :last_name)
+		end
 end
