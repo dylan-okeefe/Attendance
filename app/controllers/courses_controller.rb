cba6e9e8effc746a83f16f6bd5b813eec14a088d
dylan.okeefe <dylanokeefe@gmail.com>
2015-10-26 09:18:00 -0400

    merge ok

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,3 +1,63 @@
 class CoursesController < ApplicationController
-  
+
+  def index
+    @courses = Courses.all
+  end
+
+  def show
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def edit
+  end
+
+  def create
+    binding.pry
+    @course = Course.create(course_params)
+
+    respond_to do |format|
+      if @course.save
+        format.html { redirect_to @course, notice: 'course was successfully created.' }
+        format.json { render :show, status: :created, location: @course }
+      else
+        format.html { render :new }
+        format.json { render json: @course.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @course.update(course_params)
+        format.html { redirect_to @course, notice: 'course was successfully updated.' }
+        format.json { render :show, status: :ok, location: @course }
+      else
+        format.html { render :edit }
+        format.json { render json: @course.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @course.destroy
+    respond_to do |format|
+      format.html { redirect_to courses_url, notice: 'course was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_course
+      @course = Course.find(params[:name])
+    end
+
+
+    def course_params
+      params[:name]
+    end
+
 end
