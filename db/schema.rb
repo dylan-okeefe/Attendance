f7666b46f7c3ab53a117f6963a465460b5bcfceb
Omrika <omrikafolkes@gmail.com>
2015-10-27 13:31:13 -0400

    gravatar and styling thats going to change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151026195431) do
+ActiveRecord::Schema.define(version: 20151023155917) do
 
   create_table "attendences", force: :cascade do |t|
     t.integer  "class_id"
@@ -53,12 +53,8 @@ ActiveRecord::Schema.define(version: 20151026195431) do
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
