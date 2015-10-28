90f6e026c4a06ee8629249cab490bea9c6676d92
Omrika <omrikafolkes@gmail.com>
2015-10-28 17:55:39 -0400

    add gravatar to students page

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,7 +1,7 @@
 module StudentsHelper
    def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_id = Digest::MD5::hexdigest(current_user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.first_name, class: "gravatar")
+    image_tag(gravatar_url, alt: current_user.first_name, class: "gravatar")
   end
 end
