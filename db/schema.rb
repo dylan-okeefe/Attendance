9ad666c09cd442a8a111072c01ae5c15267c3ae4
Omrika <omrikafolkes@gmail.com>
2015-11-02 09:04:44 -0500

    i forgot, this was from like days ago.

--- db/schema.rb
+++ db/schema.rb
@@ -43,16 +43,10 @@ ActiveRecord::Schema.define(version: 20151029191303) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "course_locations", force: :cascade do |t|
-    t.integer  "course_id"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "courses", force: :cascade do |t|
     t.string   "name"
+    t.float    "latitude"
+    t.float    "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -67,14 +61,6 @@ ActiveRecord::Schema.define(version: 20151029191303) do
   create_table "instructors", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "notes", force: :cascade do |t|
-    t.integer  "student_id"
-    t.integer  "teacher_id"
-    t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
