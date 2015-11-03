70a12186272ba65caab052c67ce7aa9f2593bf2d
dylan.okeefe <dylanokeefe@gmail.com>
2015-11-03 17:06:26 -0500

    partial table

---
+++ app/datatables/student_datatable.rb
@@ -0,0 +1,33 @@
+class StudentDatatable < AjaxDatatablesRails::Base
+
+  def sortable_columns
+    # Declare strings in this format: ModelName.column_name
+    @sortable_columns ||= [Student.first_name Student.last_name]
+  end
+
+  def searchable_columns
+    # Declare strings in this format: ModelName.column_name
+    @searchable_columns ||= []
+  end
+
+  private
+
+  def data
+    records.map do |record|
+      [
+        record.first_name
+        record.last_name
+        record.attendences
+        # comma separated list of the values for each cell of a table row
+        # example: record.attribute,
+      ]
+    end
+  end
+
+  def get_raw_records
+    Student.all
+    # insert query here
+  end
+
+  # ==== Insert 'presenter'-like methods below if necessary
+end
