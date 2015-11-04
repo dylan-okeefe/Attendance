e7531d39ab9c6c4dd2a05a7b23b9e5cce03f6117
Omrika <omrikafolkes@gmail.com>
2015-11-04 14:29:18 -0500

    fix gravatar. no longer breaking

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -4,4 +4,10 @@ module StudentsHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: current_user.first_name, class: "gravatar")
   end
+
+  def gravatar_for(admin)
+    gravatar_id = Digest::MD5::hexdigest(current_admin.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: current_admin.first_name, class: "gravatar")
+  end
 end
