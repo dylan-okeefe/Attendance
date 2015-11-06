c047cf8fd3733affb25692ff4b5fdaa640912e96
BrunaNett <beedeveloping@gmail.com>
2015-11-06 09:22:49 -0500

    operation revive tree: success

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,21 @@ class Student < ActiveRecord::Base
   has_many :courses, through: :student_courses
   has_many :notes
   belongs_to :user
+
+
+  def is_late?
+    t = Time.now
+    late_range = Range.new(
+              Time.local(t.year, t.month, t.day, 9, 01),
+              Time.local(t.year, t.month, t.day, 9, 15)
+              )
+    t == late_range
+  end
+
+  def render_json
+  	json_data = {}
+  	json_data['name'] = self.first_name
+  	json_data
+  end
+
 end
